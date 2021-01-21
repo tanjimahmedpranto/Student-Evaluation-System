baa93f96b8315f2b5d33a1a4a971332567874176
tanjimahmedpranto <tanjim.outcast@gmail.com>
2021-01-22 01:25:44 +0600

    input code commented out

--- php/mark.processor.php
+++ php/mark.processor.php
@@ -13,38 +13,38 @@ if (mysqli_connect_error()) {
     if (isset($_GET['selectCourse'])) {
         $selectCourse = null;
     }
-    // $studentID = $_GET["studentID"];
-    // $courseID = $_GET["courseID"];
-    // $section = $_GET["section"];
-    // $semester = $_GET["semester"];
-    // $midQ1 = $_GET["midQ1"];
-    // $midQ2 = $_GET["midQ2"];
-    // $midQ3 = $_GET["midQ3"];
-    // $midQ4 = $_GET["midQ4"];
-    // $midQ5 = $_GET["midQ5"];
-    // $midQ6 = $_GET["midQ6"];
-    // $midTermTotal = $_GET["midTermTotal"];
-    // $finalQ1 = $_GET["finalQ1"];
-    // $finalQ2 = $_GET["finalQ2"];
-    // $finalQ3 = $_GET["finalQ3"];
-    // $finalQ4 = $_GET["finalQ4"];
-    // $finalTermTotal = $_GET["finalTermTotal"];
-    // $finalTermConverted = $_GET["finalTermConverted"];
-    // $labCO4 = $_GET["labCO4"];
-    // $projectWork = $_GET["projectWork"];
-    // $TotalCO1 = $_GET["TotalCO1"];
-    // $TotalCO2 = $_GET["TotalCO2"];
-    // $TotalCO3 = $_GET["TotalCO3"];
-    // $TotalCO4 = $_GET["TotalCO4"];
+    $studentID = $_GET["studentID"];
+    $courseID = $_GET["courseID"];
+    $section = $_GET["section"];
+    $semester = $_GET["semester"];
+    $midQ1 = $_GET["midQ1"];
+    $midQ2 = $_GET["midQ2"];
+    $midQ3 = $_GET["midQ3"];
+    $midQ4 = $_GET["midQ4"];
+    $midQ5 = $_GET["midQ5"];
+    $midQ6 = $_GET["midQ6"];
+    $midTermTotal = $_GET["midTermTotal"];
+    $finalQ1 = $_GET["finalQ1"];
+    $finalQ2 = $_GET["finalQ2"];
+    $finalQ3 = $_GET["finalQ3"];
+    $finalQ4 = $_GET["finalQ4"];
+    $finalTermTotal = $_GET["finalTermTotal"];
+    $finalTermConverted = $_GET["finalTermConverted"];
+    $labCO4 = $_GET["labCO4"];
+    $projectWork = $_GET["projectWork"];
+    $TotalCO1 = $_GET["TotalCO1"];
+    $TotalCO2 = $_GET["TotalCO2"];
+    $TotalCO3 = $_GET["TotalCO3"];
+    $TotalCO4 = $_GET["TotalCO4"];
 
-    // $query = "INSERT INTO `studentinfo` (`selectCourse`, `studentID`, `courseID`, `section`, `semester`, `midQ1`, `midQ2`, `midQ3`, `midQ4`, `midQ5`, `midQ6`, `midTermTotal`, `finalQ1`, `finalQ2`, `finalQ3`, `finalQ4`, `finalTermTotal`, `fianlTermConverted`, `labCO4`, `projectWork`, `TotalCO1`, `TotalCO2`, `TotalCO3`, `TotalCO4`) VALUES ('$selectCourse','$studentID', '$courseID', '$section', '$semester', '$midQ1', '$midQ2', '$midQ3', '$midQ4', '$midQ5', '$midQ6', '$midTermTotal', '$finalQ1', '$finalQ2', '$finalQ3', '$finalQ4', '$finalTermTotal', '$finalTermConverted', '$labCO4', '$projectWork', '$TotalCO1', '$TotalCO2', '$TotalCO3', '$TotalCO4')";
-    // $query_next = "INSERT INTO `studentinfo` (`selectCourse`, `studentID`, `courseID`, `section`) VALUES ('$selectCourse','$studentID', '$courseID', '$section')";
+    $query = "INSERT INTO `studentinfo` (`selectCourse`, `studentID`, `courseID`, `section`, `semester`, `midQ1`, `midQ2`, `midQ3`, `midQ4`, `midQ5`, `midQ6`, `midTermTotal`, `finalQ1`, `finalQ2`, `finalQ3`, `finalQ4`, `finalTermTotal`, `fianlTermConverted`, `labCO4`, `projectWork`, `TotalCO1`, `TotalCO2`, `TotalCO3`, `TotalCO4`) VALUES ('$selectCourse','$studentID', '$courseID', '$section', '$semester', '$midQ1', '$midQ2', '$midQ3', '$midQ4', '$midQ5', '$midQ6', '$midTermTotal', '$finalQ1', '$finalQ2', '$finalQ3', '$finalQ4', '$finalTermTotal', '$finalTermConverted', '$labCO4', '$projectWork', '$TotalCO1', '$TotalCO2', '$TotalCO3', '$TotalCO4')";
+    $query_next = "INSERT INTO `studentinfo` (`selectCourse`, `studentID`, `courseID`, `section`) VALUES ('$selectCourse','$studentID', '$courseID', '$section')";
     $query_test = "INSERT INTO `studentinfo` (`selectCourse`) VALUES ('$selectCourse')";
     mysqli_query($link, $query_test);
 
     // echo 'data submitted';
 
-    // header("refresh:0.0000000000000000000000000000000000000000000000000001;url=http://localhost/dars/iub_admin.php?");
+
 
     // mysqli_close($link);
 }
@@ -52,7 +52,7 @@ if (mysqli_connect_error()) {
 <!DOCTYPE html>
 <html lang="en">
 <script>
-    // window.location.replace("http://localhost/stdtmg/student-input.php?");
+    
 </script>
 
 <head>
